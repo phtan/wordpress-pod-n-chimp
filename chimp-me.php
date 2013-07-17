029fc0d28abc18a326b95fa06e63b9fb44ea189f
Pheng Heong <phengheong@hotmail.com>
2013-07-17 13:23:22 +0800

    Add failing stub for POST request from MailChimp.

--- chimp-me.php
+++ chimp-me.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: ChimpMe
-Description: A plug-in that syncs the unsubscribes from a MailChimp mailing list with a table in the WordPress database.
+Description: A WordPress plug-in that acts on Webhook events raised by a MailChimp mailing list. 
 Version: 0.1
 Author: Pheng Heong, Tan
 Author URI: https://plus.google.com/106730175494661681756
@@ -11,9 +11,13 @@ $dbName = "wordpress";
 $dbTableName = "wp_users";
 $dbEmailColumn = "user_email";
 
-// TODO Stub in a POST request here. (simulate one from MailChimp).
-// The trigger can be the 'admin_footer' hook from WordPress.
-// As example, See the first code block at http://codex.wordpress.org/AJAX_in_Plugins
+$debug = true;
+
+if ($debug) {
+	stubInPostRequest();
+}
+
+logRequest();
 
 if (!empty($_POST)) {
 
@@ -24,15 +28,75 @@ if (!empty($_POST)) {
 	// unsubscribe from database
 }
 
+/*
+ * Helper method to simulate a POST request fired by thewebhook
+ * at MailChimp.
+ */
+function stubInPostRequest() {
+	add_action('admin_footer', 'fireUnsubRequest');
+}
+
 
 
 /*
-Helper method to inspect incoming POST requests.
-Deposits a CSV file in this plug-in directory.
+ * Simulates a POST request from an MailChimp unsubscribe webhook.
+ */
+function fireUnsubRequest() {
+
+	$unsubscriberEmail = 'user1@echoandhere.com';
+	$requestTarget = __FILE__; // Post back to this file.
+
+	// Sample unsubscribe request:
+	// 
+	// type: unsubscribe
+	// fired_at: 2013-07-02 09:58:09
+    // data: {"action": "unsub", "reason": "manual", "id": "8249032551", 
+    // "email": "phtan90@gmail.com", "email_type": "html", 
+    // "ip_opt": "137.132.202.66", "web_id": "17701457", 
+    // "campaign_id": "6fb66e1caf", "merges": {"EMAIL": "phtan90@gmail.com", 
+    // "FNAME": "PH", "LNAME": "at Gmail"}, "list_id": "b970dd90fa"}
+
+    echo "Beginning Javascript POST request..."; // debug
+?>
+
+<script type="text/javascript">
+
+console.log("Starting JQuery..."); // debug.
+
+jQuery(document).ready(function($) {
+
+	console.log("Hi " + <?php echo "\"" . $requestTarget . "\""; ?>); // debug.
+ 
+	var request = {
+		type: 'unsubscribe',
+		fired_at: $.now(),
+		data: {action: "unsub", reason: "manual", id: "8249032551", 
+ 			email: <?php echo "\"" . $unsubscriberEmail . "\"" ?>, email_type: "html", 
+     		ip_opt: "137.132.202.66", web_id: "17701457", 
+     		campaign_id: "6fb66e1caf",
+     		merges: {EMAIL: <?php echo "\"" . $unsubscriberEmail . "\"" ?>, FNAME: "PH", LNAME: "at Gmail"},
+     		list_id: "b970dd90fa"}
+	};
+
+	$.post(<?php echo $requestTarget; ?>, request, function(reply) {
+		alert("Sent POST request and received the reply: " + reply);
+	});
+});
+</script>
+
+<?php
+echo "Finished running Javascript."; // debug
+}
+
+/*
+ * Helper method to inspect incoming POST requests.
+ * Deposits a CSV file in this plug-in directory.
 */
 function logRequest() {
 
-	$file = fopen("POST_request.csv", "w");
+	echo "Logging..."; // debug
+
+	$file = fopen("POST_request_log.csv", "w");
 
 	foreach ($_POST as $key => $value) {
 		$keyValuePair = array($key, $value);
