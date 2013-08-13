2e1e5bcc30aa32ab3da2f75206f55456220b61e4
Pheng Heong <phengheong@hotmail.com>
2013-08-13 15:58:49 +0800

    Initial commit of main plugin file.

---
+++ podsnchimp.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Pods 'n Chimp
+Description: A two-way sync between the Pods plug-in and MailChimp.
+Version: 0.1
+Author: Pheng Heong, Tan
+Author URI: https://plus.google.com/106730175494661681756
+*/
+
+?>
