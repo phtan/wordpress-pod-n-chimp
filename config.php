072c0cbacdb4fdba17cb0c95ade6529d0ce315dd
Pheng Heong <phengheong@hotmail.com>
2013-08-16 10:45:05 +0800

    Set up placeholders for user config.

--- config.php
+++ config.php
@@ -4,48 +4,67 @@
  * This file contains constants used throughout the Pod 'n Chimp plug-in.
  */
 
-// ====================
-// Database parameters
-// ====================
+// =============================================================================
+// SET THESE
+// =============================================================================
 
-$dbTableName = "wp_pods_contact";
-$dbEmailColumn = "email";
-$dbUnsubscribeColumn = "newsletter";
-$dbUnsubscribeBit = 0; // This value represents an unsubscribe.
+// Your MailChimp details.
+$pc_mailChimpAPIKey = 'YOUR-API-KEY';
+$pc_mailChimpListID = 'YOUR-LIST-ID'; // The list you wish to sync with.
 
-// ====================
-// Pods CMS parameters
-// ====================
+// Pods details.
+$podName = 'contact'; // Pod to sync with.
+$podEmailField = 'email'; // Name of the Pod field containing email addresses.
 
-// Details for the Pod of interest.
-$podName = 'contact';
-$podEmailField = 'email';
+// WordPress database details.
+$dbTableName = "wp_pods_contact"; // Name of the database table for the Pod.
+$dbUnsubscribeColumn = "newsletter"; // The column tracking unsubscribes.
+$dbUnsubscribeBit = 0; // The value representing an unsubscribe.
 
-// Universal Pods contants.
+// Webhook security.
+// Ensure the below values are specified with MailChimp as part of the
+// webhook callback URL.
+// 
+// For example, the callback URL should look like
+// http://send.callback.to/this-plugin.php?authenticateKey=authenticateValue)
+$authenticateKey = "ANY-NAME-FOR-YOUR-KEY";
+$authenticateValue = "HELLO-FROM-MAILCHIMP"; // Alphanumeric sequence recommended.
+
+// =============================================================================
+// OPTIONS FOR MAILCHIMP
+// =============================================================================
+
+// Whether subscribers who are new to MailChimp will receive an opt-in email.
+// Recommended by MailChimp guidelines.
+$pc_doubleOptIn = true;
+
+ // Whether to send a welcome email if the double opt-in above is not enabled.
+$pc_sendWelcome = true;
+
+ // Whether to send a notification email to the address defined in the List's
+ // email notification settings.
+$pc_unsubscribeNotifications = true;
+
+ // Email type preference for the email ('html' or 'text'). MailChimp's
+ // default is 'html'.
+$pc_defaultEmailPreference = 'html';
+
+// =============================================================================
+// THAT'S ALL
+// =============================================================================
+
+
+$dbEmailColumn = $podEmailField;
 $podsDataKey = 'fields';
 
-// ====================
-// MailChimp constants
-// ====================
-
 // Constants for the MailChimp admin to set.
-
-// $pc_mailChimpAPIKey = 'YOUR-API-KEY-HERE'; // Un-comment this line and delete the line 2 lines below.
-// $pc_mailChimpListID = 'YOUR-LIST-ID-HERE'; // Un-comment this line and delete the line 2 lines below.
-$pc_mailChimpAPIKey = 'e4f4cfc633fea7d08a90da053719685e-us7';
-$pc_mailChimpListID = 'b970dd90fa';
 
 $pc_grouping_countriesOfInterest = "Countries of interest";
 $pc_grouping_organization1 = "Organization 1";
 $pc_grouping_organization2 = "Organization 2";
 $pc_grouping_organization3 = "Organization 3";
 
-$pc_doubleOptIn = true; // Whether subscribers who are new to MailChimp will receive an opt-in email. Recommended by MailChimp guidelines.
-$pc_sendWelcome = true; // Whether to sends a welcome email instead, if double opt-in is not enabled.
-$pc_unsubscribeNotifications = true; // Whether to send a notification email to the address defined in the list email notification settings.
-$pc_defaultEmailPreference = 'html'; // Email type preference for the email ('html' or 'text'). MailChimp's default is 'html'.
-
-// Developer constants.
+// MailChimp
 $webhookEventKey = 'type'; // key as in array key.
 $unsubscribeEvent = 'unsubscribe';
 $updateEvent = 'profile';
@@ -59,9 +78,7 @@ $newEmailKey = 'new_email';
 $pc_firstNameMerge = 'FNAME';
 $pc_lastNameMerge = 'LNAME';
 
-// ===========
 // Debugging
-// ============
 $chimpme_debug = true; // set to false in production to remove verbose logs.
 
 ?>
