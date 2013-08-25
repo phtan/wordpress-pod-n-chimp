28c002d1a8505ca3418bad72d2d3bcd30f42d299
Pheng Heong <phengheong@hotmail.com>
2013-08-25 20:05:19 +0800

    Initial commit of queue class. Intention: check the queue before contacting MailChimp in the Pods post-save filter function, so that MailChimp is not contacted again if the source of the Pods-save is from MailChimp itself. First step to address bug #8.

---
+++ helpers/PCRequestQueue.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file implements a queue for incoming callbacks from the MailChimp
+ * webhook.
+ */
+
+class PCRequestQueue {
+	
+	public static function getInstance() {
+
+	}
+
+	public function add($email) {
+
+	}
+
+	public function isInQueue($email, $timeAllowance = 0) {
+
+	}
+
+	public function markDone($email) {
+
+	}
+
+	public function markNotDone($email) {
+		
+	}
+}
+
+
+?>
